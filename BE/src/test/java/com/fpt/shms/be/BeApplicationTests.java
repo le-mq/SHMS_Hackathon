af9afb04f038af16b5a7de0af17d6b4a0e52157e
Quang Le <lmq3107@gmail.com>
2026-06-05 14:12:07 +0700

    Initial commit: Setup App

---
+++ BE/src/test/java/com/fpt/shms/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.shms.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
