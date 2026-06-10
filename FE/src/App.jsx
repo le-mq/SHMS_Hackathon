1f9d926c1159c18fff52203067377d3a9627792e
Nguyen Trung Anh <nguyentrunganh13032005@gmail.com>
2026-06-10 18:27:53 +0700

    da xong verify email

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,27 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import VerifyEmail from "./components/VerifyEmail";
+import { Routes, Route } from 'react-router-dom';
+import VerifyEmail from './components/VerifyEmail'
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Navigate
-            to="/verify-email"
-            replace
-            state={{
-              username: "testuser",
-              canResendImmediately: true,
-            }}
-          />
-        }
-      />
-
+      <Routes>
       <Route path="/verify-email" element={<VerifyEmail />} />
-      <Route path="/register" element={<h1>Register Page</h1>} />
-      <Route path="/login" element={<h1>Login Page</h1>} />
-    </Routes>
+      </Routes>
   );
 }
 
