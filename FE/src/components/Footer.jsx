25ea16305393f537a37f1905dabdd0a10691d285
Quang Le <lmq3107@gmail.com>
2026-06-10 17:15:22 +0700

    done navbar, footer

---
+++ FE/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="ph-footer">
+            <div className="ph-footer-inner">
+                <div className="ph-footer-brand">S-HMS | <span>SEAL Hackathon</span></div>
+                <p className="ph-footer-copy">
+                    &copy; 2026 · Software Engineering Hackathon Management System<br/>
+                    FPT University HCMC. ALl rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
+}
