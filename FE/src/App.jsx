999ca8b2a03d27f3507cf79b213732b36861dc88
Quang Le <lmq3107@gmail.com>
2026-06-14 17:35:30 +0700

    done judge-navbar

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import PublicHome from './components/PublicHome'
@@ -10,6 +9,8 @@ import StudentProfile from './components/StudentProfile'
 import AdminProfile from './components/AdminProfile'
 import RankingsConsole from './components/RankingsConsole'
 import Leaderboard from './components/LeaderboardDashboard.jsx'
+// import EvaluationWorkspace from './components/EvaluationWorkspace.jsx'
+import EvaluatorDashboard from './components/EvaluatorDashboard.jsx'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path="/student/profile" element={<StudentProfile />} />
         <Route path="/admin/profile" element={<AdminProfile />} />
         <Route path="/admin/rankings" element={<RankingsConsole />} />
+        <Route path="/judge/workspace" element={<EvaluatorDashboard />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
       <Footer />
