ecad9b756a11f9ed06faa33672c6794d07c398b4
NgDu5 <duode0405@gmail.com>
2026-06-15 09:40:23 +0700

    add judge and mentor assignment repositories

---
+++ BE/src/main/java/com/fpt/shms/be/repository/MentorAssignmentRepository.java
@@ -0,0 +1,19 @@
+package com.fpt.shms.be.repository;
+
+import com.fpt.shms.be.model.MentorAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MentorAssignmentRepository extends JpaRepository<MentorAssignment, Long> {
+    @Query("select ma from MentorAssignment ma where ma.mentor.id = :userId")
+    List<MentorAssignment> findByUserId(Long userId);
+
+    @Modifying
+    @Query("delete from MentorAssignment ma where ma.mentor.id = :userId")
+    void deleteByUserId(Long userId);
+}
