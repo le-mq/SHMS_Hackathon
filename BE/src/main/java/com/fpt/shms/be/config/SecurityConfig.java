1e8e51b4bf23b8a320ad7647ec51d7031ed6bbf8
NgDu5 <duode0405@gmail.com>
2026-06-16 16:54:33 +0700

    du-Update Authorize for Swagger

--- BE/src/main/java/com/fpt/shms/be/config/SecurityConfig.java
+++ BE/src/main/java/com/fpt/shms/be/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
+                                "/v3/api-docs",
                                 "/swagger-ui.html"
                         ).permitAll()
                         // Public home endpoint
