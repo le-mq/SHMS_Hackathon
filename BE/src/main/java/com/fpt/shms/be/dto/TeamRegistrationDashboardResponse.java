c5d47d8e748b0c603c65a3c21487b058346a01e1
NgDu5 <duode0405@gmail.com>
2026-06-17 17:16:36 +0700

    add TeamRegistrationDashboardResponse DTO and logic in TeamService

---
+++ BE/src/main/java/com/fpt/shms/be/dto/TeamRegistrationDashboardResponse.java
@@ -0,0 +1,53 @@
+package com.fpt.shms.be.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamRegistrationDashboardResponse {
+    private List<ContestData> contests;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ContestData {
+        private Long id;
+        private String name;
+        private int pendingReview;
+        private int approved;
+        private int totalParticipants;
+        private List<CategoryCapacity> capacities;
+        private List<TeamData> teams;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CategoryCapacity {
+        private String categoryName;
+        private int percentage;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TeamData {
+        private Long id;
+        private String name;
+        private String track;
+        private String trackClass;
+        private String date;
+        private String status;
+    }
+
+}
