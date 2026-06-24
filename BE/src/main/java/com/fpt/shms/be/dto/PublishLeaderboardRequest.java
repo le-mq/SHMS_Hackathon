7d09e117eb3ef78f3f0b14920ffe5b2a32bef3d6
NgDu5 <duode0405@gmail.com>
2026-06-24 17:17:54 +0700

    du- remove category from public ranking

--- BE/src/main/java/com/fpt/shms/be/dto/PublishLeaderboardRequest.java
+++ BE/src/main/java/com/fpt/shms/be/dto/PublishLeaderboardRequest.java
@@ -1,6 +1,5 @@
 package com.fpt.shms.be.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,10 +8,8 @@ public class PublishLeaderboardRequest {
     @NotNull(message = "Contest ID is required")
     private Long contestId;
 
-    private Long categoryId;
-
-    @NotBlank(message = "Round name is required")
-    private String roundName;
+    @NotNull(message = "Round ID is required")
+    private Long roundId;
 
     private int topN;
 }
